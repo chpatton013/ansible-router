4022190d991f2f69aa4dc4b129dd67d9f71d672d
Christopher Patton <chpatton013@gmail.com>
2019-06-30 13:54:17 -0700

    Fix pip bootstrapping

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
 
 if [[ "$(id --user)" != 0 ]]; then
   echo This script must be run as root! Exiting. >&2
@@ -11,13 +11,39 @@ if [ -z "${SETUP_LOG_FILE:-}" ]; then
 fi
 echo Writing output to log file $SETUP_LOG_FILE
 
-(
-  if ! which python &>/dev/null || ! which pip &>/dev/null; then
-    apt-get update
-    apt-get install --assume-yes python python-pip
+function get_python() {
+  if which python &>/dev/null; then
+    return
   fi
+  echo Installing Python...
+  apt-get update
+  apt-get install --assume-yes python
+}
 
+function _get_pip_major_version() {
+  pip --version | sed --regexp-extended --expression='s#^pip ([0-9]+)\..*$#\1#'
+}
+
+function get_pip() {
+  if which pip &>/dev/null && [[ "$(_get_pip_major_version)" -ge 19 ]]; then
+    return
+  fi
+  echo Installing Pip...
+  curl https://bootstrap.pypa.io/get-pip.py | bash
+}
+
+function get_dependencies() {
+  echo Installing dependencies...
   pip install --requirement requirements.txt
+}
 
+function run_playbook() {
   ANSIBLE_FORCE_COLOR=true ansible-playbook playbook.yaml "$@"
+}
+
+(
+  get_python
+  get_pip
+  get_dependencies
+  run_playbook "$@"
 ) |& tee "$SETUP_LOG_FILE"
